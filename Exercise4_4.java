dcc0e17747dbec17b3586f99a162e14ee1276634
250541020 Sıla Ağgül <250541020@firat.edu.tr>
2025-12-03 21:25:43 +0300

    Update Exercise4_4.java

--- Exercise4_4.java
+++ Exercise4_4.java
@@ -12,9 +12,14 @@ public class Exercise4_4 {
 
         // 1) Donus degerini kullanmayin -> ne oluyor?
         getNumber();
+         // Cevap: Return edilen değer kullanılmadı, derleyici hata vermez.
+        // Program normal çalışır, sadece return edilen değer kaybolur.
+
 
         // 2) void metodu ifade icinde kullanin -> ne oluyor?
         // System.out.println(sayHello() + 7);
+        // Derleyici hata verir: "bad operand types for binary operator '+'"
+        // Çünkü void bir değer değildir ve toplama işlemi yapılamaz
 
         // Cevaplarinizi yorum olarak ekleyin.
     }
