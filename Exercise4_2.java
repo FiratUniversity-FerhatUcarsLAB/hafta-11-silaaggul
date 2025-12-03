85e9b5fc4956f0efbed3cc85510ec325d2c8b3b1
250541020 Sıla Ağgül <250541020@firat.edu.tr>
2025-12-03 21:14:31 +0300

    Update Exercise4_2.java

--- Exercise4_2.java
+++ Exercise4_2.java
@@ -2,22 +2,22 @@ public class Exercise4_2 {
 
   // Her satirin sonuna calisma sirasi yorumlarini ekleyin.
 
-    public static void main(String[] args) {
-        zippo("rattle", 13);
+    public static void main(String[] args) {   // 1
+        zippo("rattle", 13);                   // 2
     }
 
     public static void baffle(String blimp) {
-        System.out.println(blimp);
-        zippo("ping", -5);
+        System.out.println(blimp);            // 4
+        zippo("ping", -5);                    // 5
     }
 
     public static void zippo(String quince, int flag) {
-        if (flag < 0) {
-            System.out.println(quince + " zoop");
+        if (flag < 0) {                                    // 6
+            System.out.println(quince + " zoop");          // 7
         } else {
-            System.out.println("ik");
-            baffle(quince);
-            System.out.println("boo-wa-ha-ha");
+            System.out.println("ik");                      // 3
+            baffle(quince);                                // 8
+            System.out.println("boo-wa-ha-ha");            // 9
         }
     }
 }
