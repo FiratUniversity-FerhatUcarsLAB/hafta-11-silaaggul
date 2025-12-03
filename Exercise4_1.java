1438b3426996b399a5bf772789d79b4b540a20de
250541020 Sıla Ağgül <250541020@firat.edu.tr>
2025-12-03 21:00:53 +0300

    Update Exercise4_1.java

--- Exercise4_1.java
+++ Exercise4_1.java
@@ -1,14 +1,23 @@
 public class Exercise4_1 {
 
     public static void printAmerican(String day, int date, String month, int year) {
-        // TODO
+        System.out.println(day + ", " + month + " " + date + ", " + year);
     }
 
     public static void printEuropean(String day, int date, String month, int year) {
-        // TODO
+        System.out.println(date + " " + month + " " + year + ", " + day);
     }
 
     public static void main(String[] args) {
-        // TODO: test methods
+        String day = "Monday";
+        String month = "July";
+        int date = 22;
+        int year = 2019;
+
+        System.out.println("Amerikan Formatı");
+        printAmerican(day, date, month, year);
+
+        System.out.println("Avrupa Formatı");
+        printEuropean(day, date, month, year);
     }
 }
